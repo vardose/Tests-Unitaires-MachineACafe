70290844eec64c149c0f2c605f90736ae2246428
vardose <giraudonaxel@gmail.com>
2026-06-08 14:43:14 +0200

    init du projet

---
+++ CoinCode.cs
@@ -0,0 +1,17 @@
+﻿namespace Hardware;
+
+/// <summary>
+/// Représente les différentes pièces reconnues par la machine.
+/// Les valeurs sont en centimes d’euro.
+/// </summary>
+public enum CoinCode
+{
+    OneCent = 1,
+    TwoCents = 2,
+    FiveCents = 5,
+    TenCents = 10,
+    TwentyCents = 20,
+    FiftyCents = 50,
+    OneEuro = 100,
+    TwoEuros = 200
+}
---
+++ IChangeMachine.cs
@@ -0,0 +1,29 @@
+﻿namespace Hardware;
+
+public interface IChangeMachine
+{
+    /// <summary>
+    /// Enregistre un callback, qui sera appelé lors de l'insertion d'une pièce reconnue valide.
+    /// Attention : si le monnayeur est physiquement plein (plus de 5 pièces), cette méthode n'est plus invoquée.
+    /// Il est de la responsabilité du logiciel de surveiller cela.
+    /// </summary>
+    /// <param name="callback">Callback prenant en paramètre la valeur de la pièce détectée.</param>
+    void RegisterMoneyInsertedCallback(Action<CoinCode> callback);
+
+    /// <summary>
+    /// Vide le monnayeur et rend l'argent.
+    /// </summary>
+    void FlushStoredMoney();
+
+    /// <summary>
+    /// Vide le monnayeur et encaisse l'argent.
+    /// </summary>
+    void CollectStoredMoney();
+
+    /// <summary>
+    /// Fait tomber une pièce depuis le stock vers la trappe à monnaie.
+    /// </summary>
+    /// <param name="coinCode">Code de la pièce à rendre.</param>
+    /// <returns>True si la pièce était disponible, False sinon.</returns>
+    bool DropCashback(CoinCode coinCode);
+}
