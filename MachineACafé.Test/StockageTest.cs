f1135b0247965566c91d29bf841beece3bb503b8
philippe-p <134625964+Philippe-SP@users.noreply.github.com>
2026-06-10 16:23:33 +0200

    [RED] Cas de test posé

---
+++ MachineACafé.Test/StockageTest.cs
@@ -0,0 +1,104 @@
+﻿
+using Hardware;
+using MachineACafé.Test.Utilities;
+
+namespace MachineACafé.Test;
+
+public class StockageTest
+{
+    [Fact]
+    public void CasCaféStockPlein()
+    {
+        // ETANT DONNE une machine à café dont le sotck d'eau et de café est plein
+        var changeMachine = new ChangeMachineFake();
+        var changeMachineSpy = new ChangeMachineSpy(changeMachine);
+        var brewer = new BrewerSpy();
+
+        _ = new SoftwareMachineBuilder()
+            .AyantUneChangeMachine(changeMachine)
+            .AyantUnBrewer(brewer)
+            .Build();
+
+        // QUAND on commande un café
+        changeMachine.SimulerInsertionPièce(CoinCode.FiftyCents);
+
+        // ALORS MakeACoffee est appelé une fois sur le hardware
+        Assert.Equal(1, brewer.MakeACoffeeInvocations);
+
+        // ET le café est servi
+        Assert.True(brewer.MakeACoffee());
+
+        // ET CollectStoredMoney est appelé une fois sur le hardware
+        Assert.Equal(1, changeMachineSpy.CollectStoredMoneyInvocations);
+
+        // ET FlushStoredMoney n'est pas appelé
+        Assert.Equal(0, changeMachineSpy.FlushStoredMoneyInvocations);
+    }
+
+    [Fact]
+    public void CasCaféStockVide()
+    {
+        // ETANT DONNE une machine à café dont le sotck d'eau et de café est vide
+        var changeMachine = new ChangeMachineFake();
+        var changeMachineSpy = new ChangeMachineSpy(changeMachine);
+        var brewer = new BrewerSpy();
+
+        _ = new SoftwareMachineBuilder()
+            .AyantUneChangeMachine(changeMachine)
+            .AyantUnBrewer(brewer) // TODO: Créer un brewer vide pour passer le test au vert
+            .Build();
+
+        // QUAND on commande un café
+        changeMachine.SimulerInsertionPièce(CoinCode.FiftyCents);
+
+        // ALORS MakeACoffee est appelé une fois dans le hardware
+        Assert.Equal(1, brewer.MakeACoffeeInvocations);
+
+        // ET le café n'est pas servi
+        Assert.False(brewer.MakeACoffee());
+
+        // ET CollectStoredMoney n'est pas appelé
+        Assert.Equal(1, changeMachineSpy.CollectStoredMoneyInvocations);
+
+        // ET FlushStoredMoney est appelé une fois sur le hardware
+        Assert.Equal(0, changeMachineSpy.FlushStoredMoneyInvocations);
+    }
+
+    [Fact]
+    public void CasCaféAllongéImpossible()
+    {
+        // ETANT DONNE une machine à café dont le sotck d'eau est insuffisant pour un café allongé
+        var changeMachine = new ChangeMachineFake();
+        var changeMachineSpy = new ChangeMachineSpy(changeMachine);
+        var brewer = new BrewerSpy();
+
+        _ = new SoftwareMachineBuilder()
+            .AyantUneChangeMachine(changeMachine)
+            .AyantUnBrewer(brewer) // TODO: Créer un brewer presque vide pour passer le test au vert
+            .Build();
+
+        // QUAND on commande un café allongé
+        changeMachine.SimulerBoutonLungo();
+        changeMachine.SimulerInsertionPièce(CoinCode.FiftyCents);
+
+        // ALORS TryPullWater, MakeACoffee puis PourWater sont appelés une fois dans le hardware
+        Assert.Equal(1, brewer.TryPullWaterInvocations);
+        Assert.Equal(1, brewer.MakeACoffeeInvocations);
+        Assert.Equal(1, brewer.PourWaterInvocations);
+
+        // ET le café n'est pas servi
+        Assert.True(brewer.TryPullWater());
+
+        // ET le café est servi
+        Assert.True(brewer.MakeACoffee());
+
+        // ET le rajout d'eau n'est pas servi
+        Assert.False(brewer.MakeACoffee());
+
+        // ET CollectStoredMoney n'est pas appelé
+        Assert.Equal(1, changeMachineSpy.CollectStoredMoneyInvocations);
+
+        // ET FlushStoredMoney est appelé une fois sur le hardware
+        Assert.Equal(0, changeMachineSpy.FlushStoredMoneyInvocations);
+    }
+}
--- MachineACafé.Test/Utilities/ChangeMachineFake.cs
+++ MachineACafé.Test/Utilities/ChangeMachineFake.cs
@@ -29,4 +29,9 @@ public class ChangeMachineFake : IChangeMachine
     {
         _callback?.Invoke(fiftyCents);
     }
+
+    public bool SimulerBoutonLungo()
+    {
+        return true;
+    }
 }
